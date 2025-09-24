9978d229a70e5abfb52dc6cf67375944e3e68fde
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-09-24 17:10:57 +0530

    create a URls

--- phase-4.2/day-1-2/demo/app1/views.py
+++ phase-4.2/day-1-2/demo/app1/views.py
@@ -3,19 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Home Page')
+# def home(request):
+#     return HttpResponse('Home Page')
 
-def learn_django(request):
-    return HttpResponse('Hello Django')
+# def learn_django(request):
+#     return HttpResponse('Hello Django')
 
-def learn_python(request):
-    return HttpResponse('<h1>Hello python<h1/>')
+# def learn_python(request):
+#     return HttpResponse('<h1>Hello python<h1/>')
 
-def learn_math(request):
-    a = 10 + 10
-    return HttpResponse(a)
+# def learn_math(request):
+#     a = 10 + 10
+#     return HttpResponse(a)
 
-def learn_php(request):
-    lang = '<h1>Hello PHP<h1/>'
-    return HttpResponse(lang)
+# def learn_php(request):
+#     lang = '<h1>Hello PHP<h1/>'
+#     return HttpResponse(lang)
+
+# URL Configuration with additional parameters
+def learn_django(request, **kwargs):
+    status = kwargs.get('status', 'inactive')
+    return HttpResponse(f'<h1>Hello Django {status} - App1<h1/>')
