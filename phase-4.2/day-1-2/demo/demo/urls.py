2118733a64c87a43e1dced3fdf83cd1e4ad2bd28
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-09-24 17:11:53 +0530

    create a URls

--- phase-4.2/day-1-2/demo/demo/urls.py
+++ phase-4.2/day-1-2/demo/demo/urls.py
@@ -16,13 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app1 import views
+# from app1 import views
+from app1.views import learn_django 
+from app2.views import myApp2
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.home, name='home'),
+#     path('dj/', views.learn_django, name='learn_django'),
+#     path('py/', views.learn_python, name='learn_python'),
+#     path('math/', views.learn_math, name='learn_math'),
+#     path('php/', views.learn_php, name='learn_php'),
+# ]
+
+# URL Configuration with additional parameters
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('dj/', views.learn_django, name='learn_django'),
-    path('py/', views.learn_python, name='learn_python'),
-    path('math/', views.learn_math, name='learn_math'),
-    path('php/', views.learn_php, name='learn_php'),
+    path('py/', learn_django),
+    path('dj/', learn_django, {'status': 'active'}),
+    path('app2/', myApp2),
 ]
