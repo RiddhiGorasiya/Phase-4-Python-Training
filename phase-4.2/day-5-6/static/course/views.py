4b2bf381bd41c65b2e2584de3a8020c5c546f3ed
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-10-01 17:58:43 +0530

    by default create static-course-app file & create view

---
+++ phase-4.2/day-5-6/static/course/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def learn_django(request):
+    return render(request, 'course/django.html')
+
+def learn_python(request):
+    return render(request, 'course/python.html')
